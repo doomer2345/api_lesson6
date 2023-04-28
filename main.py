8d7ce3ad71b95d202bbaaf94b20fdf96c477da32
doomer2345 <evpalov.daniil1996@gmail.com>
2023-04-28 10:14:40 +0300

    Правки

--- main.py
+++ main.py
@@ -2,20 +2,23 @@ import requests
 from dotenv import load_dotenv
 import os
 import random
+import logging
 
 
-def get_comic():
+def get_random_comic():
     url = 'https://xkcd.com/info.0.json'
     response = requests.get(url)
     response.raise_for_status()
-    last_comic = response.json()["num"]
+    response_content = response.json()
+    last_comic = response_content["num"]
     random_comic_num = random.randint(1, last_comic)
     random_comic_url = f'https://xkcd.com/{random_comic_num}/info.0.json'
     random_comic = requests.get(random_comic_url)
     random_comic.raise_for_status()
-    url_image = random_comic.json()["img"]
-    comment = random_comic.json()["alt"]
-    dowloand_image(url_image, "комикс.jpg")
+    random_comic_params = random_comic.json()
+    image_url = random_comic_params["img"]
+    comment = random_comic_params["alt"]
+    dowloand_image(image_url, "комикс.jpg")
     return comment
 
 
@@ -35,7 +38,9 @@ def get_upload_url(vk_access_key):
     }
     response = requests.get(url, params=params)
     response.raise_for_status()
-    return response.json()['response']['upload_url']
+    response_content = response.json()
+    check_response(response_content)
+    return response_content['response']['upload_url']
 
 
 def save_wall_photo(vk_access_key, vk_photo, vk_hash, vk_server):
@@ -50,7 +55,9 @@ def save_wall_photo(vk_access_key, vk_photo, vk_hash, vk_server):
     }
     response = requests.post(url, params=params)
     response.raise_for_status()
-    return response.json()['response'][0]['id'], response.json()['response'][0]['owner_id']
+    response_content = response.json()
+    check_response(response_content)
+    return response_content['response'][0]['id'], response_content['response'][0]['owner_id']
 
 
 def send_photo_to_server(upload_url):
@@ -59,8 +66,10 @@ def send_photo_to_server(upload_url):
             'photo': file,
         }
         response = requests.post(upload_url, files=files)
-        response.raise_for_status()
-        return response.json()['hash'],response.json()['photo'],response.json()['server']
+    response.raise_for_status()
+    response_photo_params = response.json()
+    check_response(response_photo_params)
+    return response_photo_params['hash'], response_photo_params['photo'], response_photo_params['server']
 
 
 def upload_photo_to_wall(vk_media_id, vk_owner_id, vk_access_key):
@@ -75,17 +84,27 @@ def upload_photo_to_wall(vk_media_id, vk_owner_id, vk_access_key):
     }
     response = requests.post(url, params=params)
     response.raise_for_status()
-    return response.json()
+    response_content = response.json()
+    check_response(response_content)
+    return response_content
 
+
+def check_response(response_content):
+    if response_content.get("error"):
+        raise requests.exceptions.HTTPError
 
 if __name__ == '__main__':
     load_dotenv()
-    comment = get_comic()
+    comment = get_random_comic()
     vk_group_id = os.getenv("VK_GROUP_ID")
     vk_access_key = os.getenv("VK_ACCESS_TOKEN")
-    upload_url = get_upload_url(vk_access_key)
-    vk_hash,vk_photo,vk_server = send_photo_to_server(upload_url)
-    vk_media_id, vk_owner_id = save_wall_photo(vk_access_key,vk_photo, vk_hash, vk_server)
-    upload_photo_to_wall(vk_media_id, vk_owner_id, vk_access_key)
-    os.remove('комикс.jpg')
+    try:
+        upload_url = get_upload_url(vk_access_key)
+        vk_hash, vk_photo, vk_server = send_photo_to_server(upload_url)
+        vk_media_id, vk_owner_id = save_wall_photo(vk_access_key,vk_photo, vk_hash, vk_server)
+        upload_photo_to_wall(vk_media_id, vk_owner_id, vk_access_key)
+    except requests.exceptions.HTTPError:
+        logging.exception("Ошибка при запросе ВК")
+    finally:
+        os.remove('комикс.jpg')
 
